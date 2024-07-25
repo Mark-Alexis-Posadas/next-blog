fff8dacfa4a82da20acadb004dff1df54e142fc1
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-07-25 15:41:28 +0800

    added active color theme

--- app/_components/nav.tsx
+++ app/_components/nav.tsx
@@ -4,9 +4,11 @@ import { useToggle } from "../_hooks/useToggle";
 import { navbarData } from "../_data/navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const { toggle, handleSetToggle } = useToggle();
+  const router = usePathname;
   return (
     <nav className="p-3 flex items-center justify-center border-b border-slate-300 bg-slate-50 dark:bg-black">
       <ul className="flex items-center gap-3">
@@ -17,10 +19,15 @@ export default function Navbar() {
           />
         </button>
         {navbarData.map((data) => (
-          <li key={data.id}>
-            <Link href={data.path} className="dark:text-white">
-              {data.title}
-            </Link>
+          <li
+            key={data.id}
+            className={`${
+              router() === data.path
+                ? "text-purple-600 dark:text-purple-600"
+                : ""
+            } dark:text-white`}
+          >
+            <Link href={data.path}>{data.title}</Link>
           </li>
         ))}
       </ul>
